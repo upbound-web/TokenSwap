b53142d9f6bad3f5d4b78ec3d218d066005af7ec
Jake Dawson <jake@upbound.com.au>
2021-04-23 07:11:35 +1000

    added testnet - not working yet

--- truffle-config.js
+++ truffle-config.js
@@ -1,5 +1,6 @@
 require('babel-register')
 require('babel-polyfill')
+const mnemonic = fs.readFileSync('.secret').toString().trim()
 
 module.exports = {
   networks: {
@@ -7,6 +8,17 @@ module.exports = {
       host: '127.0.0.1',
       port: 7545,
       network_id: '*', // Match any network id
+    },
+    testnet: {
+      provider: () =>
+        new HDWalletProvider(
+          mnemonic,
+          `https://data-seed-prebsc-1-s1.binance.org:8545`
+        ),
+      network_id: 97,
+      confirmations: 10,
+      timeoutBlocks: 200,
+      skipDryRun: true,
     },
   },
   contracts_directory: './src/contracts/',
